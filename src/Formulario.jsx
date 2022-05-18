fc070278cdfe1e605b93f0d88db837316352d5b1
mmuyuy <mmuyuy@unal.edu.co>
2022-05-18 12:42:24 -0500

    se crea componente formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 export function Formulario() {
   return (
+    <div class="container">
+      <div class="abs-center">
     <form>
         <div>
+        <h2>
+          <br />
+          <strong>Importante:</strong> Este formulario solicita nombre, apellido y correo para saber mas de ti
+        </h2>
+        <br />
+            <label for="">Nombre</label>
+            <input class="form-control form-control-lg" type="text" placeholder="Nombre" aria-label="nombre"/>
+            <label for="">Apellido</label>
+            <input class="form-control" type="text" placeholder="Apellido" aria-label="Apellido"/>
             <label for="exampleInputEmail">Correo</label>
-            <input type="email" id="exampleInput"/>
+            <input class="form-control form-control-sm" type="email" placeholder="Correo@gmail.com" aria-label="Correo"></input>
+            <br />
             <button type="submit">Enviar</button>
         </div>
     </form>
+    </div>
+    </div>
   )
 }
